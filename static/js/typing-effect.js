823afe99f07509a774f49e9d250559eb95985d0b
Philipp Warmer <philipp.warmer@d-one.ai>
2024-10-19 22:41:14 +0200

    updated Tech in Switzerland

--- static/js/typing-effect.js
+++ static/js/typing-effect.js
@@ -1,33 +1,49 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const text = "Tech in Switzerland";
+    const initialText = "Tech from Switzerland";
+    const newText = "Innovation from Switzerland";
     const typedTextSpan = document.getElementById("typed-text");
     const cursorSpan = document.querySelector(".cursor");
 
     const initialDelay = 100; // Delay before typing starts (in milliseconds)
-    const typingSpeed = 40; // Increased typing speed for slower effect (in milliseconds)
+    const typingSpeed = 40; // Typing speed (in milliseconds)
+    const pauseDuration = 4000; // 4-second pause
     let charIndex = 0;
 
-    function typeCharacter() {
-        if (charIndex < text.length) {
-            typedTextSpan.textContent += text.charAt(charIndex);
+    function typeInitialText() {
+        if (charIndex < initialText.length) {
+            typedTextSpan.textContent += initialText.charAt(charIndex);
             charIndex++;
-            setTimeout(typeCharacter, typingSpeed);
+            setTimeout(typeInitialText, typingSpeed);
+        } else {
+            setTimeout(startOverwriting, pauseDuration);
+        }
+    }
+
+    function startOverwriting() {
+        charIndex = 0;
+        overwriteText();
+    }
+
+    function overwriteText() {
+        if (charIndex < newText.length) {
+            typedTextSpan.textContent = newText.slice(0, charIndex + 1) + initialText.slice(charIndex + 1);
+            charIndex++;
+            setTimeout(overwriteText, typingSpeed);
         } else {
             // Typing complete, remove cursor after a short delay
             setTimeout(() => {
                 cursorSpan.style.display = 'none';
-            }, 1000); // Cursor remains visible for 1 second after typing
+            }, 1000);
         }
     }
 
     function startTyping() {
         typedTextSpan.textContent = '';
         cursorSpan.style.display = 'inline-block';
-        setTimeout(typeCharacter, initialDelay);
+        setTimeout(typeInitialText, initialDelay);
     }
 
     if (typedTextSpan && cursorSpan) {
         startTyping();
     }
 });
-
